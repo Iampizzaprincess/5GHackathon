233aa1766b3ae08e7be1478406a0c1e9e141bfdc
dsrasheed <daniyaalrasheed@gmail.com>
2021-10-23 02:50:25 -0400

    Fixed requirements.txt

--- backend/app/bets/views.py
+++ backend/app/bets/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from app.bets.model import Bet
 
 bets_blueprint = Blueprint(
@@ -6,18 +6,22 @@ bets_blueprint = Blueprint(
 )
 
 @bets_blueprint.route('/', methods=['POST'])
-def create_bet():
+def create():
     b = Bet("Woohoo")
     db.session.add(v)
     db.session.commit()
     return {'success': True}
 
+@bets_blueprint.route('/', methods=['GET'])
+def get_all():
+    bets = Bet.query.all()
+    bets = {i:bet for i,bet in enumerate(bets)}
+    return bets if bets != {} else "I am empty inside"
+
 @bets_blueprint.route('/<id>', methods=['GET'])
 def get_bet(id):
-    if not id:
-        bets = Bet.query.all()
-        return bets
-    else:
-        bet = Bet.query.filter_by(id=id).first()
-        return bet
+    bet = Bet.query.filter_by(id=id).first()
+    if bet is None:
+        return "Fuck you"
+    return bet
 
