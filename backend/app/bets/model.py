ba726e1cc838d5a3880972c6a4ff6a4824b766e1
dsrasheed <daniyaalrasheed@gmail.com>
2021-10-23 10:00:01 -0400

    Added dummy data

--- backend/app/bets/model.py
+++ backend/app/bets/model.py
@@ -7,6 +7,12 @@ class Bet(db.Model):
     
     def __init__(self, description):
         self.description = description
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'description': self.description
+        }
 
     def __repr__(self):
         return f'<Bet {self.id}: {self.description[:20]}'
